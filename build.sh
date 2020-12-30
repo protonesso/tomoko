7aea455fce9f530b8aaacb69607ce64f82c8325b
Ishimoto Shinobu <nagakamira@gmail.com>
2020-12-31 01:04:33 +0900

    updates
    Signed-off-by: Ishimoto Shinobu <nagakamira@gmail.com>

--- build.sh
+++ build.sh
@@ -6,15 +6,109 @@ set -e
 main() {
 	export MUSLVER="1.2.1"
 	export LLVMVER="11.0.0"
+	export LINUXVER="5.10"
 
 	export LANG=C
 	export LC_ALL=C
+	export HOSTCC=clang
+	export HOSTCXX=clang++
 	export STUFF="$PWD/stuff"
 	export BUILD="$PWD/build"
 	export SRCDIR="$BUILD/src"
 	export TOOLS="$BUILD/tools"
 	export SYSROOT="$BUILD/sysroot"
 	export PATH="$TOOLS/bin:$PATH"
+
+	case "$1" in
+		x86_64)
+			export XTARGET="x86_64-linux-musl"
+			export LTARGET="X86"
+			export LARCH="x86_64"
+			export MARCH="x86_64"
+			export KARCH="x86_64"
+			;;
+		i386)
+			export XTARGET="i686-linux-musl"
+			export LTARGET="X86"
+			export LARCH="x86"
+			export MARCH="i386"
+			export KARCH="i386"
+			;;
+		aarch64)
+			export XTARGET="aarch64-linux-musl"
+			export LTARGET="Aarch64"
+			export LARCH="aarch64"
+			export MARCH="aarch64"
+			export KARCH="arm64"
+			;;
+		armv7l)
+			export XTARGET="armv7a-linux-musleabihf"
+			export LTARGET="ARM"
+			export LARCH="arm"
+			export MARCH="arm"
+			export KARCH="arm"
+			;;
+		armv6l)
+			export XTARGET="armv6a-linux-musleabihf"
+			export LTARGET="ARM"
+			export LARCH="arm"
+			export MARCH="arm"
+			export KARCH="arm"
+			;;
+		armv5te)
+			export XTARGET="armv5te-linux-musleabi"
+			export LTARGET="ARM"
+			export LARCH="arm"
+			export MARCH="arm"
+			export KARCH="arm"
+			;;
+		mips64)
+			export XTARGET="mips64-linux-musl"
+			export LTARGET="Mips"
+			export LARCH="mips64"
+			export MARCH="mips64"
+			export KARCH="mips"
+			;;
+		mips64el)
+			export XTARGET="mips64el-linux-musl"
+			export LTARGET="Mips"
+			export LARCH="mips64el"
+			export MARCH="mips64"
+			export KARCH="mips"
+			;;
+		mips)
+			export XTARGET="mips-linux-musl"
+			export LTARGET="Mips"
+			export LARCH="mips"
+			export MARCH="mips"
+			export KARCH="mips"
+			;;
+		mipsel)
+			export XTARGET="mipsel-linux-musl"
+			export LTARGET="Mips"
+			export LARCH="mipsel"
+			export MARCH="mips"
+			export KARCH="mips"
+			;;
+		s390x)
+			export XTARGET="s390x-linux-musl"
+			export LTARGET="SystemZ"
+			export LARCH="s390x"
+			export MARCH="s390x"
+			export KARCH="s390"
+			;;
+		riscv64)
+			export XTARGET="riscv64-linux-musl"
+			export LTARGET="RISCV"
+			export LARCH="riscv64"
+			export MARCH="riscv64"
+			export KARCH="riscv"
+			;;
+		*)
+			echo "Specify CPU architecture"
+			exit 1
+			;;
+	esac
 
 	rm -rvf "$BUILD"
 	mkdir -pv "$SRCDIR" "$TOOLS" "$SYSROOT"
@@ -28,9 +122,27 @@ main() {
 	pushd "$SRCDIR/libcxx-$LLVMVER.src"
 		patch -Np1 -i "$STUFF"/libcxx-01-musl-hardfix.patch
 	popd
+	pushd "$SRCDIR/libcxxabi-$LLVMVER.src"
+		patch -Np1 -i "$STUFF"/libcxxabi_musl_exit.patch
+	popd
 
 	curl -C - -L -O https://musl.libc.org/releases/musl-$MUSLVER.tar.gz
 	bsdtar -xvf musl-$MUSLVER.tar.gz
+
+	curl -C - -L -O https://cdn.kernel.org/pub/linux/kernel/v5.x/linux-$LINUXVER.tar.xz
+	bsdtar -xvf linux-$LINUXVER.tar.xz
+
+	cd "$SRCDIR"/linux-$LINUXVER
+	make mrproper -j$(nproc)
+
+	make ARCH=$KARCH headers -j$(nproc)
+	mkdir -p "$SYSROOT"/usr/include
+
+	find usr/include -name '.*' -delete
+	rm usr/include/Makefile
+	cp -a usr/include/* "$SYSROOT"/usr/include
+
+	find "$SYSROOT" \( -name .install -o -name ..install.cmd \) -print0 | xargs -0 rm -rf
 
 	cd "$SRCDIR"/llvm-$LLVMVER.src
 	cp -av "$SRCDIR"/clang-$LLVMVER.src tools/clang
@@ -61,9 +173,8 @@ main() {
 		-DCOMPILER_RT_BUILD_PROFILE=OFF \
 		-DCOMPILER_RT_BUILD_SANITIZERS=OFF \
 		-DCOMPILER_RT_BUILD_XRAY=OFF \
-		-DCOMPILER_RT_USE_BUILTINS_LIBRARY=ON \
-		-DCOMPILER_RT_USE_LIBCXX=ON \
 		-DLIBCXX_CXX_ABI=libcxxabi \
+		-DLIBCXX_INCLUDE_TESTS=OFF \
 		-DLIBCXX_USE_COMPILER_RT=ON \
 		-DLIBCXXABI_USE_LLVM_UNWINDER=ON \
 		-DLIBCXXABI_USE_COMPILER_RT=ON \
@@ -71,9 +182,9 @@ main() {
 		-DLLVM_BUILD_EXAMPLES=OFF \
 		-DLLVM_BUILD_DOCS=OFF \
 		-DLLVM_BUILD_TESTS=OFF \
-		-DLLVM_TARGET_ARCH=Mips \
-		-DLLVM_TARGETS_TO_BUILD=Mips \
-		-DLLVM_DEFAULT_TARGET_TRIPLE=mipsel-linux-musl \
+		-DLLVM_TARGET_ARCH=$LARCH \
+		-DLLVM_TARGETS_TO_BUILD=$LTARGET \
+		-DLLVM_DEFAULT_TARGET_TRIPLE=$XTARGET \
 		-DDEFAULT_SYSROOT="$SYSROOT" \
 		-Wno-dev -G Ninja
 	ninja -j$(nproc)
@@ -83,48 +194,50 @@ main() {
 
 	pushd "$TOOLS/bin"
 		for i in cc c++ clang clang++ cpp; do
-			cp -v $realclang mipsel-linux-musl-$i
+			cp -v $realclang $XTARGET-$i
 		done
 
 		for i in ar as dwp nm objcopy objdump size strings; do
-			cp -v llvm-$i mipsel-linux-musl-$i
+			cp -v llvm-$i $XTARGET-$i
 		done
 
-		cp -v lld mipsel-linux-musl-ld
-		cp -v lld mipsel-linux-musl-ld.lld
-		cp -v llvm-symbolizer mipsel-linux-musl-addr2line
-		cp -v llvm-cxxfilt mipsel-linux-musl-c++filt
-		cp -v llvm-ar mipsel-linux-musl-ranlib
-		cp -v llvm-readobj mipsel-linux-musl-readelf
-		cp -v llvm-objcopy mipsel-linux-musl-strip
+		cp -v lld $XTARGET-ld
+		cp -v lld $XTARGET-ld.lld
+		cp -v llvm-symbolizer $XTARGET-addr2line
+		cp -v llvm-cxxfilt $XTARGET-c++filt
+		cp -v llvm-ar $XTARGET-ranlib
+		cp -v llvm-readobj $XTARGET-readelf
+		cp -v llvm-objcopy $XTARGET-strip
 
 		rm -fv $realclang clang clang++ clang-cl clang-cpp \
 			lld-link ld.lld ld64.lld wasm-ld lld
 	popd
 
 	cd "$SRCDIR"/musl-$MUSLVER
-	make ARCH=mips prefix=/usr DESTDIR="$SYSROOT" install-headers
+	make ARCH=$MARCH prefix=/usr DESTDIR="$SYSROOT" install-headers -j$(nproc)
 
 	cd "$SRCDIR"
 	mkdir -p compiler-rt-builtins-build
 	cd compiler-rt-builtins-build
-	cmake ../compiler-rt-$LLVMVER.src \
-		-DCMAKE_C_COMPILER_TARGET="mipsel-linux-musl" \
-		-DCMAKE_ASM_COMPILER_TARGET="mipsel-linux-musl" \
-		-DCMAKE_C_COMPILER="$TOOLS/bin/mipsel-linux-musl-clang" \
-		-DCMAKE_CXX_COMPILER="$TOOLS/bin/mipsel-linux-musl-clang++" \
-		-DCMAKE_AR="$TOOLS/bin/mipsel-linux-musl-ar" \
-		-DCMAKE_NM="$TOOLS/bin/mipsel-linux-musl-nm" \
-		-DCMAKE_RANLIB="$TOOLS/bin/mipsel-linux-musl-ranlib" \
+	cmake "$SRCDIR"/compiler-rt-$LLVMVER.src \
+		-DCMAKE_C_COMPILER_TARGET="$XTARGET" \
+		-DCMAKE_ASM_COMPILER_TARGET="$XTARGET" \
+		-DCMAKE_C_COMPILER="$TOOLS/bin/$XTARGET-clang" \
+		-DCMAKE_CXX_COMPILER="$TOOLS/bin/$XTARGET-clang++" \
+		-DCMAKE_AR="$TOOLS/bin/$XTARGET-ar" \
+		-DCMAKE_NM="$TOOLS/bin/$XTARGET-nm" \
+		-DCMAKE_RANLIB="$TOOLS/bin/$XTARGET-ranlib" \
 		-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
 		-DLLVM_CONFIG_PATH="$TOOLS/bin/llvm-config" \
 		-DCOMPILER_RT_DEFAULT_TARGET_ONLY=ON \
 		-DCOMPILER_RT_BUILD_BUILTINS=ON \
+		-DCOMPILER_RT_BUILD_LIBFUZZER=OFF \
+		-DCOMPILER_RT_BUILD_PROFILE=OFF \
 		-DCOMPILER_RT_BUILD_SANITIZERS=OFF \
 		-DCOMPILER_RT_BUILD_XRAY=OFF \
 		-DCOMPILER_RT_INCLUDE_TESTS=OFF \
 		-Wno-dev -G Ninja
-	ninja
+	ninja -j$(nproc)
 
 	for i in lib/linux/*; do
 		install -Dm644 "$i" "$TOOLS"/lib/clang/$LLVMVER/lib/linux/$(basename $i)
@@ -133,41 +246,63 @@ main() {
 	cd "$SRCDIR"/musl-$MUSLVER
 	./configure \
 		--build=$(clang -dumpmachine) \
-		--host=mipsel-linux-musl \
-		--target=mipsel-linux-musl \
+		--host=$XTARGET \
+		--target=$XTARGET \
 		--prefix=/usr \
 		--libdir=/usr/lib \
 		--syslibdir=/usr/lib \
 		--enable-optimize=size \
-		LIBCC="$(mipsel-linux-musl-clang -print-libgcc-file-name)"
-	make -j9
-	make DESTDIR="$SYSROOT" install -j9
+		LIBCC="$($XTARGET-clang -print-libgcc-file-name)"
+	make -j$(nproc)
+	make DESTDIR="$SYSROOT" install -j$(nproc)
 
-	touch "$SYSROOT"/usr/lib/crt{begin,end}S.o
+	pushd "$SRCDIR"
+		for i in libunwind libcxx libcxxabi openmp; do
+			ln -svf $i-$LLVMVER.src $i
+		done
+	popd
 
-	cd "$SRCDIR"/libunwind-$LLVMVER.src
+	cd "$SRCDIR"/llvm-$LLVMVER.src
+	rm -rf build
 	mkdir -p build
 	cd build
-	cmake "$SRCDIR/libunwind-$LLVMVER.src" \
+	cmake "$SRCDIR/llvm-$LLVMVER.src" \
+		-DCMAKE_CROSSCOMPILING=ON \
 		-DCMAKE_INSTALL_PREFIX=/usr \
 		-DCMAKE_BUILD_TYPE=MinSizeRel \
-		-DCMAKE_C_COMPILER_TARGET="mipsel-linux-musl" \
-		-DCMAKE_ASM_COMPILER_TARGET="mipsel-linux-musl" \
-		-DCMAKE_C_COMPILER="$TOOLS/bin/mipsel-linux-musl-clang" \
-		-DCMAKE_CXX_COMPILER="$TOOLS/bin/mipsel-linux-musl-clang++" \
-		-DCMAKE_AR="$TOOLS/bin/mipsel-linux-musl-ar" \
-		-DCMAKE_NM="$TOOLS/bin/mipsel-linux-musl-nm" \
-		-DCMAKE_RANLIB="$TOOLS/bin/mipsel-linux-musl-ranlib" \
+		-DCMAKE_C_COMPILER_TARGET="$XTARGET" \
+		-DCMAKE_ASM_COMPILER_TARGET="$XTARGET" \
+		-DCMAKE_C_COMPILER="$TOOLS/bin/$XTARGET-clang" \
+		-DCMAKE_CXX_COMPILER="$TOOLS/bin/$XTARGET-clang++" \
+		-DCMAKE_AR="$TOOLS/bin/$XTARGET-ar" \
+		-DCMAKE_NM="$TOOLS/bin/$XTARGET-nm" \
 		-DCMAKE_TRY_COMPILE_TARGET_TYPE=STATIC_LIBRARY \
 		-DLLVM_CONFIG_PATH="$TOOLS/bin/llvm-config" \
+		-DLIBCXX_CXX_ABI=libcxxabi \
+		-DLIBCXX_HAS_MUSL_LIBC=ON \
+		-DLIBCXX_INCLUDE_TESTS=OFF \
+		-DLIBCXX_USE_COMPILER_RT=ON \
+		-DLIBCXX_TARGET_TRIPLE=$XTARGET \
+		-DLIBCXXABI_USE_LLVM_UNWINDER=ON \
+		-DLIBCXXABI_USE_COMPILER_RT=ON \
+		-DLIBCXXABI_TARGET_TRIPLE=$XTARGET \
 		-DLIBUNWIND_USE_COMPILER_RT=ON \
-		-DLIBUNWIND_TARGET_TRIPLE="mipsel-linux-musl" \
+		-DLIBUNWIND_TARGET_TRIPLE=$XTARGET \
+		-DLLVM_BUILD_EXAMPLES=OFF \
+		-DLLVM_BUILD_DOCS=OFF \
+		-DLLVM_BUILD_TESTS=OFF \
+		-DLLVM_ENABLE_PROJECTS="libunwind;libcxx;libcxxabi" \
+		-DLLVM_TARGETS_TO_BUILD=$LTARGET \
+		-DLLVM_DEFAULT_TARGET_TRIPLE=$XTARGET \
 		-Wno-dev -G Ninja
-	ninja
-	DESTDIR="$SYSROOT" ninja install
+
+	sed -i 's/-latomic//g' build.ninja
+
+	ninja unwind cxx -j$(nproc)
+	DESTDIR="$SYSROOT" ninja install-unwind install-cxxabi install-cxx -j$(nproc)
 }
 
-main
+main "$1"
 
 exit 0
 
